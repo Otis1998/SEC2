8e4d5cf87f6a382c296129c0b86e43f9191f7235
DESKTOP-I4AVOEQ\asus <161250149@smail.nju.edu.cn>
2019-06-09 12:04:20 +0800

    debug

--- sql/cinema.sql
+++ sql/cinema.sql
@@ -343,7 +343,7 @@ UNLOCK TABLES;
 --
 -- Table structure for table `recharge_present`
 --
-DROP TABLE IF EXISTS `recharge_present`
+DROP TABLE IF EXISTS `recharge_present`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `recharge_present`(
